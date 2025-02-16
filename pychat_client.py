94fa013d155b6d22a15fe8e6c334366a7d66cef7
Mr_duck <93455237+gustavorochaduck@users.noreply.github.com>
2025-02-16 13:12:27 +0100

    upgrade on the SERVER

---
+++ pychat_client.py
@@ -0,0 +1,5 @@
+import cli_func as fc
+server_host = input('The IP adress from the Server: ')
+server_port = input('The Port from the Server: ')
+
+fc.acssi_client_art()
--- pychat_server.py
+++ pychat_server.py
@@ -7,3 +7,13 @@ ADDR = (SERVER, PORT)
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STRAM)
 server.bind(ADDR)
+
+def handle_client(conn, addr):
+    pass
+
+def start():
+    server.listen()
+    while True:
+        conn, addr = server.accept()
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        print(f"[ACTIVE CONNECTIONS] {thread.activeCount() -1}")
